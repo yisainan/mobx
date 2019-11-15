45fcda6700c4cf8bca893dd5d6df3ba90f23e782
qiilee <qiilee@126.com>
2019-11-15 09:59:25 +0800

    fix: 2-2 decorator修饰器语法

--- mobx-test/src/index.js
+++ mobx-test/src/index.js
@@ -1,17 +1,74 @@
-class Animal {
-    name () {
-        return 'Animal'
-    }
-    say () {
-        return `I am ${this.name}`
+// 2-1代码
+
+// class Animal {
+//     name () {
+//         return 'Animal'
+//     }
+//     say () {
+//         return `I am ${this.name}`
+//     }
+// }
+
+// class Dog extends Animal {
+//     food = '123';
+//     name () {
+//         return 'Dog'
+//     }
+// }
+
+// console.log(new Dog() instanceof Animal)
+
+
+// 2-2代码
+function log(target) {
+    const desc = Object.getOwnPropertyDescriptors(target.prototype)
+
+    for (const key of Object.keys(desc)) {
+        if (key === 'constructor') {
+            continue
+        }
+        const func = desc[key].value;
+
+        if ('function' === typeof func) {
+            Object.defineProperty(target.prototype, key,{
+                value(...args){
+                    console.log('before ' + key)
+                    const ret = func.apply(this, args);
+                    console.log('after ' + key)
+
+                    return ret
+                }
+            })
+        }
     }
 }
 
-class Dog extends Animal {
-    food = '123';
-    name () {
-        return 'Dog'
+function readonly(target, key, descriptor){
+    descriptor.writable = false
+}
+
+function validate(target, key, descriptor){
+    const func = descriptor.value;
+    descriptor.value = function (...args) {
+        for (let num of args) {
+            if ('number' != typeof num) {
+                throw new Error(`${num} is not a number`)
+            }
+        }
+        return func.apply(this, args)
     }
 }
 
-console.log(new Dog() instanceof Animal)
+@log // 类修饰器 
+class Numberic {
+    @readonly PI = 3.1415926; // 类属性成员修饰器
+
+    @validate // 类方法成员修饰器
+    add(...nums) {
+        return nums.reduce((p, n) => (p + n), 0)
+    }
+}
+
+new Numberic().add(1, 2) // 正确打印
+// new Numberic().PI = 100 // 控制台报错 Uncaught TypeError: Cannot assign to read only property 'PI' of object '#<Numberic>'
+new Numberic().add(1, 'x') // 控制台报错 index.js:55 Uncaught Error: x is not a number
